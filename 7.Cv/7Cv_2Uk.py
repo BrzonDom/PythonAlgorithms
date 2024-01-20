df08e65495bb89a4977178ebc6a4968c2b37a9aa
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-20 14:49:13 +0100

    7.Cv v1.1

---
+++ 7.Cv/7Cv_2Uk.py
@@ -0,0 +1,49 @@
+"""
+Výpočet Fibonacciho posloupnosti
+
+    Napište rekurzivní výpočet Fibonacciho posloupnosti. Pro tuto posloupnost platí:
+
+        F_n = F_n−1 + F_n−2
+
+    První členy posloupnosti jsou
+
+        F_0 = 0 a F_1 = 1.
+
+"""
+
+print("Fibonacci sequence:")
+
+n = 8
+
+Fib_sol = [[0, 0], [1, 1]]
+Fib_list = [n]
+Fib_len = len(Fib_list)
+
+# for num in range(Fib_len):
+#     if Fib_list[num] > 1:
+#         Fib_list.append((Fib_list[num] - 2))
+#         Fib_list[num] -= 1
+#         Fib_len = len(Fib_list)
+#     else:
+#         continue
+
+
+num = 0
+while num < Fib_len:
+    if Fib_list[num] > 1:
+        Fib_list.append((Fib_list[num] - 2))
+        Fib_list[num] -= 1
+        Fib_len = len(Fib_list)
+        num = 0
+    else:
+        num += 1
+
+Fib_num = 0
+for num in Fib_list:
+    Fib_num += num
+
+print(f"\tF({n}) = {Fib_num}")
+
+
+
+
