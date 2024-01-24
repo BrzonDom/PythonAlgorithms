3d65e33a811ef0d77f45500f9039ce6c3a05735c
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-24 17:49:10 +0100

    4.DU_L v1.1

--- DU/4.DU/4.DU_L_1.py
+++ DU/4.DU/4.DU_L_1.py
@@ -46,54 +46,105 @@ Příklady:
     Výstup:
         4 0 0
         
-
 """
 
-str_seq11 = "1 2 3 3 3 3 3 3 3 3 5 6"
-str_seq12 = "3 3 3 1 3 3 3 3 3 3 3 3"
+str_seq_list = [["1 2 3 3 3 3 3 3 3 3 5 6", "3 3 3 1 3 3 3 3 3 3 3 3"],
+                ["10", "1 2 3 10 3 2 1"],
+                ["1 2 3 4", "1 2 3 4"]]
 
-seq11 = [int(num) for num in list(str_seq11.split(" "))]
-seq12 = [int(num) for num in list(str_seq12.split(" "))]
+str_seq = str_seq_list[0]
+str_seq1 = str_seq[0]
+str_seq2 = str_seq[1]
 
-len_seq11 = len(seq11)
-len_seq12 = len(seq12)
+seq1 = [int(num) for num in list(str_seq1.split(" "))]
+seq2 = [int(num) for num in list(str_seq2.split(" "))]
+seq = [seq1, seq2]
 
-LCS_Mat = [[0 for i in range(len_seq11+1)] for j in range(len_seq12+1)]
+len_seq1 = len(seq1)
+len_seq2 = len(seq2)
 
-for row in LCS_Mat:
-    print(row)
+print("Sequenses:")
+print(f"\tLen: {len_seq1} Seq: {seq1}")
+print(f"\tLen: {len_seq2} Seq: {seq2}")
+print()
 
-# print(LCS_Mat)
+LCS_Mat = [[0 for i in range(len_seq1 + 1)] for j in range(len_seq2 + 1)]
+
+# print()
+# for row in LCS_Mat:
+#     print(row)
+#
+# # print(LCS_Mat)
 
 result = 0
 res_index = []
-
-
-for i in range(len_seq11+1):
-    for j in range(len_seq12+1):
+#
+#
+for i in range(len_seq1 + 1):
+    for j in range(len_seq2 + 1):
         if (i == 0 or j == 0):
             LCS_Mat[i][j] = 0
-        elif (seq11[i-1] == seq12[j-1]):
+        elif (seq1[i - 1] == seq2[j - 1]):
             LCS_Mat[i][j] = LCS_Mat[i-1][j-1] + 1
             if LCS_Mat[i][j] > result:
                 res_index = [i, j]
             result = max(result, LCS_Mat[i][j])
         else:
             LCS_Mat[i][j] = 0
+#
+# print()
+#
+for i in range(len_seq1 + 1):
+    for j in range(len_seq2 + 1):
+        if i == 0 or j == 0:
+            continue
 
-print()
-
-for i in range(len_seq11+1):
-    for j in range(len_seq12+1):
         print(LCS_Mat[i][j], end=" ")
     print()
+#
+# print()
+# print(result)
+# res_index[0] -= result
+# res_index[1] -= result
+# print(res_index)
+#
+# print()
+# print(f"Sequence lenght: {result}")
+# print(f"\t\tStart index: {res_index[0]}\n\t\tEnd index:   {res_index[1]}")
 
-print()
-print(result)
-res_index[0] -= result
-res_index[1] -= result
-print(res_index)
+def LCS_2Vis(seq, Mat):
 
-print()
-print(f"Sequence lenght: {result}")
-print(f"\t\tStart index: {res_index[0]}\n\t\tEnd index:   {res_index[1]}")
+    seq1 = seq[0]
+    seq2 = seq[1]
+    lenSeq1 = len(seq1)
+    lenSeq2 = len(seq2)
+
+    print("\n\n\t   |", end="")
+    for b in range(lenSeq2):
+        print(f" {seq2[b]:2} |", end="")
+    print("\n\t", end="")
+
+    for r in range(1, lenSeq1 + 2):
+        print(" --", end="")
+        for b in range(1, lenSeq2 + 1):
+            print("+----", end="")
+        print("+\n\t", end="")
+        # print()
+
+        if r == lenSeq1+1:
+            break
+        print(f"{seq1[r-1]:2} ", end="")
+
+        for c in range(1, lenSeq2 + 1):
+
+            # print(f"| {Mat[r][c]:2} ", end="")
+
+            if LCS_Mat[r][c]:
+                print(f"| {Mat[r][c]:2} ", end="")
+            else:
+                print(f"|    ", end="")
+
+
+        print("|\n\t", end="")
+
+LCS_2Vis(seq, LCS_Mat)
