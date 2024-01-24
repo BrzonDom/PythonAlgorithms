15fcf557683744eff3859d3c8892e25befeef702
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-24 15:05:13 +0100

    8.Le v1.1

--- 8.Cv/8.Le/8Le_3Stack.py
+++ 8.Cv/8.Le/8Le_3Stack.py
@@ -12,5 +12,20 @@ stack.append(4)
 stack.append(5)
 stack.append(6)
 
+print("From last to first")
 while len(stack) > 0:
-    print(stack.pop())
+    print("\t" + str(stack.pop()))
+print()
+
+
+stack.append("first")
+stack.append("second")
+stack.append("third")
+stack.append(4)
+stack.append(5)
+stack.append(6)
+
+print("From first to last")
+while len(stack) > 0:
+    print("\t" + str(stack.pop(0)))
+print()
