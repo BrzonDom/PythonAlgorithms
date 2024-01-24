992f446223158b866a1b63d6c1ee20605eda583c
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-24 16:59:37 +0100

    3.DU_L v2.0

--- DU/3.DU/3.DU_L.py
+++ DU/3.DU/3.DU_L.py
@@ -69,11 +69,13 @@ inNum_list = [[21, 9, 8, 7, 6],
               [192779, 253, 263, 273, 283, 683, 693, 703, 713, 723, 733, 743],
               [3099, 6376, 8005, 4116505, 9980, 10000]]
 
-inNum = inNum_list[0]
+inNum = inNum_list[1]
 
 print(f"Number input: {inNum}")
 
 print(f"\n\tBase number:             {inNum[0]}")
+baseNum = inNum[0]
+
 print(f"\tNumber of numbers:       {len(inNum)}")
 print(f"\tNumber of test numbers:  {len(inNum) - 1}")
 
@@ -116,6 +118,30 @@ def primDec(num):
     # print(primDecLst)
     return primDecLst
 
+def GCD(num1, num2):
+
+    """
+        Dokud b není rovno nule
+            t = b
+            b = a mod b
+            a = t
+        a je největší společný dělitel původních čísel
+    """
+
+    a = num1
+    b = num2
+
+    steps = 0
+
+    while b != 0:
+        # steps += 1
+
+        t = b
+        b = a % b
+        a = t
+
+    # print(f"GCD.2: ({num1},{num2}) = {a}\n\tSteps: {steps}\n")
+    return a
 
 print("\nNumber decomposition:")
 
@@ -131,4 +157,22 @@ for num in inNum:
         numDecStr += f"{prim} * "
     print(numDecStr[:-3])
 
+GCD_list = []
+GCD_dict = {}
+
+print("\nGreatest common divisor:")
+for num in inNum[1:]:
+    numGCD = GCD(baseNum, num)
+    GCD_list.append(numGCD)
+    GCD_dict[numGCD] = num
+    print(f"\tGCD ({baseNum}, {num}) = {numGCD}")
+    print(f"\t\t{numGCD} : {GCD_dict[numGCD]}")
+
+maxGCD = max(GCD_list)
+maxNum = GCD_dict[maxGCD]
+
+print()
+print(f"For base number: {baseNum}")
+print(f"\tMax GCD:        {maxGCD}")
+print(f"\tNum of max GCD: {maxNum}")
 
