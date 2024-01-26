35f263c770d0d6d90e71c5e2c192702a409676f7
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-26 07:25:49 +0100

    6.Le v2.1

--- 6.Cv/6.Le/6Le_10Perm.py
+++ 6.Cv/6.Le/6Le_10Perm.py
@@ -15,8 +15,30 @@ def prtPerm(prefix, items):
     for i in range(len(items)):
         prtPerm(prefix + items[i], items[:i] + items[i+1:])
 
-# items = ['a', 'b', 'c', 'd']
-items = ['a', 'b', 'c']
+
+def perm(prefix, items):
+
+    if len(items) == 0:
+        # print(prefix, end=" ")
+        perm_list.append(prefix)
+
+    for i in range(len(items)):
+        perm(prefix + items[i], items[:i] + items[i+1:])
+
+
+items = ['a', 'b', 'c', 'd']
+# items = ['a', 'b', 'c']
+
+perm_list = []
 
 print("Permutation", end="\n\t")
-prtPerm("", items)
+perm("", items)
+
+# print(perm_list)
+
+for i, per in enumerate(perm_list):
+    print(per, end="  ")
+
+    if (i+1) % 6 == 0:
+        print("", end="\n\t")
+# prtPerm("", items)
