8a3db7c8b9ebdfd4ebaaf16d7a22d323f1b303ff
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-26 03:10:41 +0100

    8.Cv v1.1

--- 8.Cv/8Cv_1Uk.py
+++ 8.Cv/8Cv_1Uk.py
@@ -11,3 +11,34 @@ Dekódování zprávy
         TE*A*QYS***SEU****NI*O**
 
 """
+
+coded = "TE*A*QYS***SEU****NI*O**"
+decoded = ""
+
+decode_stack = []
+stack_log = []
+i = 1
+
+print()
+print(f"Coded:   {coded}")
+
+for char in coded:
+
+    # print(f"{i:2}: {decode_stack}")
+    stack_log.append(f"{i:2}: {decode_stack}")
+    i += 1
+
+    if char == '*':
+        # print(decode_stack.pop(), end="")
+        decoded += decode_stack.pop()
+    else:
+        # decode_stack.insert(0, char)
+        decode_stack.append(char)
+
+print(f"Decoded: {decoded}")
+
+print("\nStack log:")
+
+for line in stack_log:
+    print(f"\t{line}")
+
