1e14a5715153a1ff6a320fa8daf4d1ef00f0c9f7
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-29 00:05:42 +0100

    3.Le v1.1

--- 3.Cv/3.Le/3Le_13numIndex.py
+++ 3.Cv/3.Le/3Le_13numIndex.py
@@ -1,23 +1,28 @@
 
 
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers = [0, 1, 2, 3, 4, 5, 3, 6, 7, 8, 9, 10]
 
-searNum = 0
+searNum = 3
 
 print(f"Number to be found: {searNum}")
 print(f"List of numbers: {numbers}")
 print()
 
-index = -1
+index = []
 
 for n, num in enumerate(numbers):
     # print(f"{n}. {num}")
     if num == searNum:
-        index = n
-        print(f"Num. {searNum} found at index {index}")
+        index.append(n)
+        # print(f"Num. {searNum} found at index", end=" ")
 
-if index == -1:
+if index:
+    print(f"Num. {searNum} found at index", end=" ")
+    for i in index:
+        print(i, end=" ")
+
+elif not index:
     print(f"Num. {searNum} not found")
 
 
