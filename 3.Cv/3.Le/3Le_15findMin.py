120d49e8769d86db26bbef3154c47069c9ceb8c6
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-29 01:06:52 +0100

    3.Le v1.3

--- 3.Cv/3.Le/3Le_15findMin.py
+++ 3.Cv/3.Le/3Le_15findMin.py
@@ -1,5 +1,54 @@
 
+"""
+def my_findMax(Set):
 
+    if len(Set) == 0:
+        return -1
+
+    index_list = []
+    index = 0
+    Max = Set[0]
+
+    for num in Set:
+
+        if num > Max:
+            Max = num
+            index_list = [index]
+
+        elif num == Max:
+            index_list.append(index)
+
+        index += 1
+
+    print(f"Max: {Max}\n\tIndex: ", end="")
+    for i in index_list:
+        print(i, end=" ")
+
+    return [num, index_list]
+"""
+
+def my_findMin(Set):
+
+    if len(Set) == 0:
+        return -1
+
+    index = []
+    min = Set[0]
+
+    for n, num in enumerate(Set):
+
+        if num < min:
+            min = num
+            index = [n]
+
+        elif num == min:
+            index.append(n)
+
+    print(f"Min: {min}\n\tIndex: ", end="")
+    for i in index:
+        print(i, end=" ")
+
+    return num, index
 
 
 numbers = [1, 6, 1, -1, 0, -1]
@@ -24,3 +73,14 @@ if numbers:
     print(f"Found at index: ", end="")
     for i in index:
         print(i, end=" ")
+
+print("\n")
+
+min, index = my_findMin(numbers)
+
+print("\n")
+
+print(f"Minimum: {min}")
+print(f"Found at index: ", end="")
+for i in index:
+    print(i, end=" ")
