8ac7bac5050f6b8f2e323bc2feb18fad16d9baf0
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-29 06:47:23 +0100

    4.Le v2.2

--- 4.Cv/4.Le/4Le_14sort.py
+++ 4.Cv/4.Le/4Le_14sort.py
@@ -8,6 +8,15 @@ def isSort(set):
     return True
 
 
+def bubbleSort(set):
+
+    for _ in range(len(set)):
+        for i in range(len(set) - 1):
+            if set[i] > set[i + 1]:
+                set[i], set[i + 1] = set[i + 1], set[i]
+
+
+
 set = [10, -1, 2, 0, 0]
 
 print(f"List: {set}\n")
@@ -19,12 +28,38 @@ print(set, " Sorted: ", isSort(set))
 print()
 
 set = [100, -10, 0, 1, -3, 5, 4]
-print(f"List: {set}\n")
+print(f"List: {set}")
 
-print(set)
+print("\t", set)
 
-for i in range(len(set) - 1):
-    if set[i] > set[i+1]:
-        set[i], set[i+1] = set[i+1], set[i]
+for _ in range(len(set)):
+    for i in range(len(set) - 1):
+        if set[i] > set[i+1]:
+            set[i], set[i+1] = set[i+1], set[i]
 
-print(set)
+print("\t", set)
+print()
+
+set = [100, -10, 0, 1, -3, 5, 4]
+
+print(f"List: {set}")
+
+print("\t", set)
+
+bubbleSort(set)
+
+print("\t", set)
+print()
+
+set = [100, -10, 0, 1, -3, 5, 4]
+
+print(f"List: {set}")
+
+print("\t", set)
+
+while not isSort(set):
+    for i in range(len(set) - 1):
+        if set[i] > set[i + 1]:
+            set[i], set[i + 1] = set[i + 1], set[i]
+
+print("\t", set)
