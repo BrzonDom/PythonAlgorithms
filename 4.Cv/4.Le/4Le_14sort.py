e3f83b1b48fe4fabc46cfe7901e018e15a2dd894
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-29 06:14:45 +0100

    4.Le v2.1

--- 4.Cv/4.Le/4Le_14sort.py
+++ 4.Cv/4.Le/4Le_14sort.py
@@ -9,7 +9,22 @@ def isSort(set):
 
 
 set = [10, -1, 2, 0, 0]
+
+print(f"List: {set}\n")
+
 print(set, " Sorted: ", isSort(set))
 
 set.sort()
 print(set, " Sorted: ", isSort(set))
+print()
+
+set = [100, -10, 0, 1, -3, 5, 4]
+print(f"List: {set}\n")
+
+print(set)
+
+for i in range(len(set) - 1):
+    if set[i] > set[i+1]:
+        set[i], set[i+1] = set[i+1], set[i]
+
+print(set)
