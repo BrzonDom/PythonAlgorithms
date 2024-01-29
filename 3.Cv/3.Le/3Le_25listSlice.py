17cbf5068cff87edd2522800a2368d8f7b59f8fe
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-29 04:20:09 +0100

    3.Le v1.5

--- 3.Cv/3.Le/3Le_25listSlice.py
+++ 3.Cv/3.Le/3Le_25listSlice.py
@@ -11,13 +11,29 @@ List slices
 
 """
 
-a = [1, 0, 3, "a", "!", 12, 0]
+# a = [1, 0, 3, "a", "!", 12, 0]
+#
+# print(a[3:])
+# print(a[:])
+# print(a[:-3])
+# print(a[3:-3])
+# print(a[3:4])
+# print(a[2:5])
+# print(a[1:-1])
+# print(a[4:4])
 
-print(a[3:])
-print(a[:])
-print(a[:-3])
-print(a[3:-3])
-print(a[3:4])
-print(a[2:5])
-print(a[1:-1])
-print(a[4:4])
+# list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(f"List: {list}\n")
+
+print(f"\tlist[3 :   ] = {list[3:]}")
+print(f"\tlist[  :   ] = {list[:]}")
+print(f"\tlist[  : -3] = {list[:-3]}")
+print(f"\tlist[3 : -3] = {list[3:-3]}")
+print(f"\tlist[3 :  4] = {list[3:4]}")
+print(f"\tlist[2 :  5] = {list[2:5]}")
+print(f"\tlist[1 : -1] = {list[1:-1]}")
+print(f"\tlist[4 :  4] = {list[4:4]}")
+
+
