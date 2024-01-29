d2ca9c93299c9461e91c524a2057fbfc2c1a50cd
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-29 04:58:18 +0100

    4.Le v1.3

--- 4.Cv/4.Le/4Le_5linSearch.py
+++ 4.Cv/4.Le/4Le_5linSearch.py
@@ -29,20 +29,54 @@ def findItms(set, theItm):
 
 a = [0, 1, 0, 2]
 
+print(f"List: {a}")
+print()
+
+print(f"Item: 0")
 search = findItm(a, 0)
-print(search)
+print("\t", search)
 
 if search:
     index = findItmIndx(a, 0)
-    print(index)
+    print("\t", index)
 print()
 
-print(findItm(a, '0'))
+print(f"Item: '0'")
+print("\t", findItm(a, '0'))
 print()
 
 a = ['a', 'b', 'aa', 'a', 'bb', 'a']
 
+print(f"List: {a}")
+print()
+
+print(f"Item: 'a'")
 index = findItms(a, 'a')
-print(index)
+print("\t", index)
+print()
+
+print(f"Item: 'A'")
 index = findItms(a, 'A')
-print(index)
+print("\t", index)
+
+print()
+
+print(f"Item: 'a'")
+if 'a' in a:
+    print("\t", True)
+    i = 0
+    index = []
+
+    while i < len(a):
+        if 'a' in a[i:]:
+            i = a.index('a', i)
+            index.append(i)
+            # print(index)
+            i += 1
+        else:
+            break
+
+    print("\t", index)
+
+else:
+    print("\t", False)
