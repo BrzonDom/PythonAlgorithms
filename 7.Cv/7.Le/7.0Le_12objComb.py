26794a71a9edde2dd9bb7f4b006715f34dee5c4c
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-01-31 18:18:10 +0100

    7.0.Le v1.3

--- 7.Cv/7.Le/7.0Le_12objComb.py
+++ 7.Cv/7.Le/7.0Le_12objComb.py
@@ -19,6 +19,13 @@ class Circle:
     def sumArea(self, other):
         return self.area() + other.area()
 
+    def intersect(self, other):
+
+        dx = self.x - other.x
+        dy = self.y - other.y
+
+        return (dx ** 2 + dy ** 2) ** (0.5) <= self.rad + other.rad
+
 
 
 clC1 = Circle(1, 1, 1, "green")
@@ -45,3 +52,59 @@ print("Area sum of 1.Circle and 3.Circle:\n")
 print(f"\tArea of 1.Circle = {clC1.area()}")
 print(f"\tArea of 3.Circle = {clC3.area()}")
 print(f"\n\tCombined area = {clC1.area()} + {clC3.area()} = {clC1.sumArea(clC3)}")
+print("\n")
+
+print("Do circles intersect:")
+print()
+
+crcInt = [1, 2]
+print(f"\t{crcInt[0]}. Circle and {crcInt[1]}.Circle")
+print(f"\t\t{crcInt[0]}.Circle:")
+print(f"\t\t\tCoord: [{clCirc_lst[crcInt[0]-1].x}, {clCirc_lst[crcInt[0]-1].y}]")
+print(f"\t\t\tRadius: {clCirc_lst[crcInt[0]-1].rad}")
+print(f"\t\t{crcInt[1]}.Circle:")
+print(f"\t\t\tCoord: [{clCirc_lst[crcInt[1]-1].x}, {clCirc_lst[crcInt[1]-1].y}]")
+print(f"\t\t\tRadius: {clCirc_lst[crcInt[1]-1].rad}")
+# print(f"\t\t{crcInt[1]}.Circ coord: [{clCirc_lst[crcInt[1]-1].x}, {clCirc_lst[crcInt[1]-1].y}]")
+# print(f"\t\t{crcInt[0]}.Circ radius: {clCirc_lst[crcInt[0]-1].rad}")
+# print(f"\t\t{crcInt[1]}.Circ radius: {clCirc_lst[crcInt[1]-1].rad}")
+if (clCirc_lst[crcInt[0]-1].intersect(clCirc_lst[crcInt[1]-1])):
+    print("\tThey DO intersect")
+else:
+    print("\tThey DON'T intersect")
+print()
+
+crcInt = [1, 3]
+print(f"\t{crcInt[0]}. Circle and {crcInt[1]}.Circle")
+print(f"\t\t{crcInt[0]}.Circle:")
+print(f"\t\t\tCoord: [{clCirc_lst[crcInt[0]-1].x}, {clCirc_lst[crcInt[0]-1].y}]")
+print(f"\t\t\tRadius: {clCirc_lst[crcInt[0]-1].rad}")
+print(f"\t\t{crcInt[1]}.Circle:")
+print(f"\t\t\tCoord: [{clCirc_lst[crcInt[1]-1].x}, {clCirc_lst[crcInt[1]-1].y}]")
+print(f"\t\t\tRadius: {clCirc_lst[crcInt[1]-1].rad}")
+# print(f"\t\t{crcInt[1]}.Circ coord: [{clCirc_lst[crcInt[1]-1].x}, {clCirc_lst[crcInt[1]-1].y}]")
+# print(f"\t\t{crcInt[0]}.Circ radius: {clCirc_lst[crcInt[0]-1].rad}")
+# print(f"\t\t{crcInt[1]}.Circ radius: {clCirc_lst[crcInt[1]-1].rad}")
+if (clCirc_lst[crcInt[0]-1].intersect(clCirc_lst[crcInt[1]-1])):
+    print("\tThey DO intersect")
+else:
+    print("\tThey DON'T intersect")
+print()
+
+crcInt = [2, 3]
+print(f"\t{crcInt[0]}. Circle and {crcInt[1]}.Circle")
+print(f"\t\t{crcInt[0]}.Circle:")
+print(f"\t\t\tCoord: [{clCirc_lst[crcInt[0]-1].x}, {clCirc_lst[crcInt[0]-1].y}]")
+print(f"\t\t\tRadius: {clCirc_lst[crcInt[0]-1].rad}")
+print(f"\t\t{crcInt[1]}.Circle:")
+print(f"\t\t\tCoord: [{clCirc_lst[crcInt[1]-1].x}, {clCirc_lst[crcInt[1]-1].y}]")
+print(f"\t\t\tRadius: {clCirc_lst[crcInt[1]-1].rad}")
+# print(f"\t\t{crcInt[1]}.Circ coord: [{clCirc_lst[crcInt[1]-1].x}, {clCirc_lst[crcInt[1]-1].y}]")
+# print(f"\t\t{crcInt[0]}.Circ radius: {clCirc_lst[crcInt[0]-1].rad}")
+# print(f"\t\t{crcInt[1]}.Circ radius: {clCirc_lst[crcInt[1]-1].rad}")
+if (clCirc_lst[crcInt[0]-1].intersect(clCirc_lst[crcInt[1]-1])):
+    print("\tThey DO intersect")
+else:
+    print("\tThey DON'T intersect")
+print()
+
