5dd1a78ad5d6825b5730f91ae55d62f6776caee0
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-01 22:37:12 +0100

    11.Le v1.1

--- 11.Cv/11.Le/11Le_0Puzzle.py
+++ 11.Cv/11.Le/11Le_0Puzzle.py
@@ -1,6 +1,30 @@
 import copy
+from queue import Queue
 
-def right(board):
+neighb_dict = {}
+
+def moves(orgBoard):
+
+    neghbBoard = []
+
+    if orgBoard != right(orgBoard):
+        neghbBoard.append(right(orgBoard))
+
+    if orgBoard != left(orgBoard):
+        neghbBoard.append(left(orgBoard))
+
+    if orgBoard != up(orgBoard):
+        neghbBoard.append(up(orgBoard))
+
+    if orgBoard != down(orgBoard):
+        neghbBoard.append(down(orgBoard))
+
+    return neghbBoard
+
+
+def right(orgBoard):
+    board = copy.deepcopy(orgBoard)
+
     for r in range(len(board)):
         for c in range(len(board[r])-1):
             if board[r][c] == 0:
@@ -9,7 +33,9 @@ def right(board):
 
     return board
 
-def left(board):
+def left(orgBoard):
+    board = copy.deepcopy(orgBoard)
+
     for r in range(len(board)):
         for c in range(1, len(board[r])):
             if board[r][c] == 0:
@@ -18,7 +44,9 @@ def left(board):
 
     return board
 
-def up(board):
+def up(orgBoard):
+    board = copy.deepcopy(orgBoard)
+
     for r in range(1, len(board)):
         for c in range(len(board[r])):
             if board[r][c] == 0:
@@ -27,7 +55,9 @@ def up(board):
 
     return board
 
-def down(board):
+def down(orgBoard):
+    board = copy.deepcopy(orgBoard)
+
     for r in range(len(board)-1):
         for c in range(len(board[r])):
             if board[r][c] == 0:
@@ -39,15 +69,35 @@ def down(board):
 org_board = [[8, 5, 6], [4, 0, 3], [1, 2, 7]]
 board = copy.deepcopy(org_board)
 
-print("Original board:")
-print()
+print("Original board:", end="\n\t")
 
-for row in board:
+for row in org_board:
     for col in row:
         print(col, end=" ")
-    print()
+    print("", end="\n\t")
 print()
 
+print("Solved board:", end="\n\t")
+slv_board = copy.deepcopy(org_board)
+
+iOrd = 0
+row = len(slv_board)
+col = len(slv_board[0])
+
+for r in range(row):
+    for c in range(col):
+        iOrd += 1
+
+        if iOrd == (row * col):
+            slv_board[r][c] = 0
+        else:
+            slv_board[r][c] = iOrd
+
+        print(slv_board[r][c], end=" ")
+    print("", end="\n\t")
+print()
+
+print("\tMoves\n")
 print("Right:", end="\n\t")
 board = right(board)
 
@@ -89,3 +139,11 @@ for row in board:
         print(col, end=" ")
     print("", end="\n\t")
 print()
+
+board = copy.deepcopy(org_board)
+
+print()
+
+other_board = left(board)
+print(board)
+print(other_board)
