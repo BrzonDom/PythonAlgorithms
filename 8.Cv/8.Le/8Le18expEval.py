b24c74b73cb3cc87bbaad1c84e697a7e1910b085
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-01 15:24:15 +0100

    8.Le v1.4

---
+++ 8.Cv/8.Le/8Le18expEval.py
@@ -0,0 +1,42 @@
+
+def expEval(ex):
+
+    stack = []
+
+    for arg in ex.split():
+        if arg == '+':
+            stack.append(stack.pop() + stack.pop())
+        elif arg == '-':
+            stack.append(-stack.pop() + stack.pop())
+        elif arg == '*':
+            stack.append(stack.pop() * stack.pop())
+        elif arg == '/':
+            sec = stack.pop()
+            frs = stack.pop()
+
+            stack.append(frs / sec)
+        else:
+            stack.append(float(arg))
+
+    return stack.pop()
+
+exp_lst = ["3 4 +",
+           "3 4 -",
+           "3 4 *",
+           "3 4 /",
+           "3 4 * 2 -",
+           "3 4 2 - *"]
+
+i = 0
+
+print(f"{i+1}.Exp:\n\t\t3 + 4 = evalExp(3 4 +) = {expEval(exp_lst[i])}")
+i += 1
+print(f"{i+1}.Exp:\n\t\t3 - 4 = evalExp(3 4 -) = {expEval(exp_lst[i])}")
+i += 1
+print(f"{i+1}.Exp:\n\t\t3 * 4 = evalExp(3 4 *) = {expEval(exp_lst[i])}")
+i += 1
+print(f"{i+1}.Exp:\n\t\t3 / 4 = evalExp(3 4 /) = {expEval(exp_lst[i])}")
+i += 1
+print(f"{i+1}.Exp:\n\t\t3 * 4 - 2 = evalExp(3 4 * 2 -) = {expEval(exp_lst[i])}")
+i += 1
+print(f"{i+1}.Exp:\n\t\t3 * (4 - 2) = evalExp(3 4 2 - *) = {expEval(exp_lst[i])}")
