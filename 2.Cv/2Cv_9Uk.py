20df6a4f596f0de201efbf892ad1d2f98d79457d
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-06 17:15:00 +0100

    2.Cv v1.2

--- 2.Cv/2Cv_9Uk.py
+++ 2.Cv/2Cv_9Uk.py
@@ -23,3 +23,66 @@ Metoda půlení intervalu hledá řešení obecné rovnice f(x)=0, kdy známe dv
     Na počátku zvolte x1=0 a x2=y pro kladná y>1, x1=y a x2=0 pro záporná y<−1, a x1=−1 a x2=1 v ostatních případech.
 
 """
+
+def f(x):
+    fnkc = (x - 1)**2 - 4
+    return(fnkc)
+
+
+def interval(a, b):
+    if f(a) < 0 and f(b) > 0:
+        return [a, b]
+    elif f(a) > 0 and f(b) < 0:
+        return [b, a]
+    else:
+        return ["error", "error"]
+
+
+def bisec_metod(a, b, e):
+
+    while abs(a - b) > e:
+        c = (a + b) / 2
+
+        if (f(c) < 0):
+            a = c
+        elif (f(c) > 0):
+            b = c
+
+    return [a, b]
+
+
+a0 = 10
+    # f(a) < 0
+b0 = 0
+    # f(b) > 0
+e = 0.001
+
+a = interval(a0, b0)[0]
+b = interval(a0, b0)[1]
+print(f"\na = {a}\nb = {b}\ne = {e}\n")
+
+c = (a + b) / 2
+
+
+# while (abs(f(a)) > 0.1 or abs(f(b)) > 0.1):
+
+while (abs(a - b) > e):
+    c = (a + b) / 2
+
+    if (f(c) < 0):
+        a = c
+    elif (f(c) > 0):
+        b = c
+
+print(f"a: {a}\t f(a): {f(a)}\nb: {b}\t f(b): {f(b)}")
+
+a = 0
+b = 10
+
+print(f"\n\na = {a}\nb = {b}\ne = {e}\n")
+
+bisec_metod(a, b, e)
+a = bisec_metod(a, b, e)[0]
+b = bisec_metod(a, b, e)[1]
+
+print(f"a: {a}\t f(a): {f(a)}\nb: {b}\t f(b): {f(b)}")
