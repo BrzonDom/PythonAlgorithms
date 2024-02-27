64fc513b4da3fb912d5169b200136335f5224286
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-27 22:09:13 +0100

    10.Le v1.3

--- 10.Cv/10.Le/10Le_9SearchWidth.py
+++ 10.Cv/10.Le/10Le_9SearchWidth.py
@@ -22,13 +22,18 @@ def Route(graph, start, goal):
 
     while len(queue) > 0:
         actual = queue.pop(0)
+        print(actual.name)
 
         if actual.name == goal:
             path = Traverse(actual)
             return path[::-1]
+
         if not actual.name in graph:
             continue
+
         for neighbor in graph[actual.name]:
+
+            print("\t", neighbor)
             if not neighbor in known:
                 known[neighbor] = True
                 queue.append(FindRoute(neighbor, actual))
